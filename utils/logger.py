29c8ad79fc16eacc8ea81e9cee93a74b6947037e
karpenkoalla1504-blip <karpenkoalla15.04@gmail.com>
2025-09-22 10:05:43 -0400

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+import logging, sys
+
+def get_logger(name="qa"):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        h = logging.StreamHandler(sys.stdout)
+        h.setFormatter(logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s"))
+        logger.addHandler(h)
+    return logger
